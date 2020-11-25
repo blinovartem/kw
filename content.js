24fb94c078657bd9b988296607e568d2f0a11505
Artem <blinow99@mail.ru>
2020-11-25 23:15:24 +0400

    reload

--- content.js
+++ content.js
@@ -1,28 +1,28 @@
+//Code by Artem Blinov - blinow99@yandex.com
+
 chrome.extension.onRequest.addListener(
     function (request, sender, sendResponse) {
         if (request.method == "getTag") {
-            let keywords = '';
-            let x = document.getElementsByClassName("oc_x_c" );
-            let i;
-
-            keywords = [keywords].map(k => k.innerText).map(a => `<a href="/search/${a}">${a}</a>` -'<br>');
+            var tempTags = '';
+            var x = document.getElementsByClassName("C_b_8978e" );
+            var i;
             for (i = 0; i < x.length; i++) {
-                keywords = x[i].innerText + ', '; 
+                tempTags = tempTags + x[i].innerHTML + ', ';
             }
-            
-            sendResponse({ data: keywords, count: x.length, method: "getTag" });
+            tempTags = tempTags.substring(0, tempTags.length - 2);
+
+            sendResponse({ data: tempTags, count: x.length, method: "getTag" });
         }
+        else if (request.method == "ExpandableKeywordsList") {
 
-        else if (request.method == "KeywordsCarousel_root_div") {
-
-            let array = document.getElementsByClassName("relatedKeywords");
-            for (let i = 0; i < array.length; i++) {
+            var array = document.getElementsByClassName("relatedKeywords");
+            for (var i = 0; i < array.length; i++) {
                 array[i].style.height = "450px";
             }
 
 
             if (array.length > 0)
-                sendResponse({ length: array.length, method: "KeywordsCarousel_root_div" });
+                sendResponse({ length: array.length, method: "ExpandableKeywordsList" });
 
             sendResponse({  });
         }

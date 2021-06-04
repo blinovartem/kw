5535238f0897d1043757da3c8173b84571f50407
Artem <blinow99@mail.ru>
2021-06-04 16:33:01 +0400

    Update April 2021

--- content.js
+++ content.js
@@ -1,19 +1,22 @@
 //Code by Artem Blinov - blinow99@yandex.com
 
-chrome.extension.onRequest.addListener(
+browser.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.method == "getTag") {
             var tempTags = '';
-            var x = document.getElementsByClassName("C_b_8978e" );
+            var x = document.getElementsByClassName("C_b_8978e");
             var i;
             for (i = 0; i < x.length; i++) {
                 tempTags = tempTags + x[i].innerHTML + ', ';
             }
             tempTags = tempTags.substring(0, tempTags.length - 2);
 
-            sendResponse({ data: tempTags, count: x.length, method: "getTag" });
-        }
-        else if (request.method == "ExpandableKeywordsList") {
+            sendResponse({
+                data: tempTags,
+                count: x.length,
+                method: "getTag"
+            });
+        } else if (request.method == "ExpandableKeywordsList") {
 
             var array = document.getElementsByClassName("relatedKeywords");
             for (var i = 0; i < array.length; i++) {
@@ -22,9 +25,12 @@ chrome.extension.onRequest.addListener(
 
 
             if (array.length > 0)
-                sendResponse({ length: array.length, method: "ExpandableKeywordsList" });
+                sendResponse({
+                    length: array.length,
+                    method: "ExpandableKeywordsList"
+                });
 
-            sendResponse({  });
+            sendResponse({});
         }
     }
 );
--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+//Code by Artem Blinov - blinow99@yandex.com
 
 // When the popup HTML has loaded
 window.addEventListener('DOMContentLoaded', function() {
@@ -8,19 +9,19 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Get Keyword
     btnGetKeyWord.onclick = function () {
-        chrome.tabs.getSelected( function (tab) {
-            chrome.tabs.sendRequest(tab.id, { method: "getTag" }, function (response) {
+        browser.tabs.query({currentWindow: true, active: true}); {
+            browser.tabs.sendMessage(tab.id, { method: "getTag" }, function (response) {
                 if (response == undefined || response.method !== "getTag" || response.data == '') {
-                    lblFeedback.innerText = chrome.i18n.getMessage("alertGetkey");
+                    lblFeedback.innerText = browser.i18n.getMessage("alertGetkey");
                     lblFeedback.className = "alert alert-danger";
                     return;
                 }
 
                 txtResult.innerText = response.data;
-                lblFeedback.innerText = chrome.i18n.getMessage("alertFound") + response.count + chrome.i18n.getMessage("alertKeywords");
+                lblFeedback.innerText = browser.i18n.getMessage("alertFound") + response.count + browser.i18n.getMessage("alertKeywords");
                 lblFeedback.className = "alert alert-success";
             });
-        });
+        };
     };
     // Function Copy
     btnCopy.onclick = function () {
@@ -31,7 +32,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
         if (lblFeedback.innerText.indexOf('Copied') == -1 && lblFeedback.innerText.indexOf('Not found words.') === -1) {
-            lblFeedback.innerText = lblFeedback.innerText + chrome.i18n.getMessage("alertCopied");
+            lblFeedback.innerText = lblFeedback.innerText + browser.i18n.getMessage("alertCopied");
             lblFeedback.className = "alert alert-success";
         }
 
